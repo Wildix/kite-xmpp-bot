f7f0cffcff26dc26a988d2a3c618d935dd3deae7
Semen Darienko <semen.darienko@wildix.com>
2021-01-28 16:37:10 +0200

    Remove garbage

--- cmake-build-debug/CMakeFiles/kite_xmpp_bot.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/kite_xmpp_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/kite_xmpp_bot.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for kite_xmpp_bot.
-# This may be replaced when dependencies are built.
